2791a284b898f5474fcf5fa2bd21c30351583cd3
nivleM-ed <melvinedward98@gmail.com>
2020-02-05 17:09:08 +0800

    Test

--- iniChain.js
+++ iniChain.js
@@ -2,10 +2,10 @@ const Blockchain = require('./modals/chain');
 const myPorts = require('./port');
 const client = require('socket.io-client');
 const socketListeners = require('./socket/socketListeners');
-// var socketApi = require('./socket/socketApi');
+var socketApi = require('./socket/socketApi');
 // var io = socketApi.io;
 
-const blockChain = new Blockchain(null, myPorts.io);
+const blockChain = new Blockchain(null, socketApi.io);
 
 function doIniBc() {
     blockChain.addNode(socketListeners(client(`http://localhost:${myPorts.port}`), blockChain));
